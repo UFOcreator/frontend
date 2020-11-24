2aacac09d0be64d2c2a9f688049663cb9aba1de5
Wesley <wesleyvtartwijk@gmail.com>
2020-11-24 16:31:05 +0100

    Dailly layout Update

--- src/Components/Dailly/Dailly.js
+++ src/Components/Dailly/Dailly.js
@@ -1,13 +1,14 @@
 import './Dailly.css';
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Dailly() {
   return (
     <div className="dailly">
-      <div className="headerImage">
-        <img src="Images/DaillyBanner.png" alt="DaillyBanner" id="imageDailly"></img>
-        <div className="content">
-          <p>Wat te doen vandaag?</p>
+      <img src="Images/DaillyBanner.png" alt="DaillyBanner" id="imageDailly"></img>
+      <div className="content">
+        <p>Wat te doen vandaag?</p>
+        <div className="Challenges">
           <ul>
             <li>Push Ups</li>
             <li>Push Ups</li>
@@ -42,9 +43,14 @@ function Dailly() {
             <li>Push Ups</li>
             <li>Push Ups</li>
           </ul>
-          
         </div>
       </div>
+      <Link to='/'>
+        <div className="lowBar">
+          <h3 className="textBottom">Dailly Challenges</h3>
+          <i className="fas fa-chevron-up"></i>
+        </div>
+      </Link>
     </div>
   );
 }
